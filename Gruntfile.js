24cb598d656dab6dc0f441bf6bc18927a2a574cc
Goncalo Espinha <gespinha@Macintosh.local>
2017-05-30 21:46:06 +0100

    changed the file structure of the repo. Updated the package.json dependencies. Added a .npmignore file and updated the .gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,11 @@
 module.exports = function (grunt) {
   'use strict';
 
-  // Project configuration
   grunt.initConfig({
-    // Metadata
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= props.license %> */\n',
-
-    // Task configuration
     postcss: {
       options: {
         processors: [
-          require('postcss-spacer')({
+          require('./task.js')({
             'comments': {
               before: 2,
               after: 2
@@ -30,9 +20,7 @@ module.exports = function (grunt) {
     }
   });
 
-  // These plugins provide necessary tasks
   grunt.loadNpmTasks('grunt-postcss');
 
-  // Default task
   grunt.registerTask('default', ['postcss']);
 };
